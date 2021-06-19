bf39679297d8ca45bbb8d9e06d7b12453770f67d
jOKERII <631854721@qq.com>
2021-06-19 23:29:34 +0800

    the first test 移植freertos至原有模板

---
+++ USER/init.c
@@ -0,0 +1,30 @@
+/**
+  ******************************************************************************
+  * @file    init.c
+  * @author  jOKERII
+  * @version v1.0
+  * @date    20-11-03
+  * @brief   System_Init(void);
+						 
+
+  ******************************************************************************
+  */
+
+#include "init.h"
+
+
+
+
+/**
+  * @brief  system init
+  * @param  none
+  * @retval нч
+  */
+void System_Init(void)
+{
+	
+
+	
+}
+
+
